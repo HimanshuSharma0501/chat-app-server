b45d8b87de1908f6d7e4eae9080ea5387839cee4
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-08-26 13:23:21 +0530

    Authentication Routes and Controllers made

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,99 @@
-export const signup = (req, res) => {
-  res.send("Sign Up");
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please Provide all the fields" });
+    }
+
+    if (password !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Passwords Did Not Match" });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already taken" });
+    }
+
+    //hash password here
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //profilepic
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${fullName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${fullName}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+    if (newUser) {
+      //Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res);
+
+      await newUser.save();
+      return res.status(200).json({
+        success: true,
+        message: `User with username: ${username} created Succesfully`,
+      });
+    } else {
+      res.status(400).json({ success: false, message: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
-export const login = (req, res) => {
-  res.send("Login");
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter all fields" });
+    }
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ success: true, message: "User not found" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password || ""
+    );
+    if (!isPasswordCorrect) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Invalid username or password" });
+    }
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      sucess: true,
+      message: "Logged In Successfully",
+    });
+  } catch (error) {
+    console.log("Error", error.message);
+  }
 };
+
 export const logout = (req, res) => {
-  res.send("Log Out");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ success: true, message: "Logged Out Successfully" });
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
--- server.js
+++ server.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 dotenv.config();
+app.use(express.json());
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Working fine on ${PORT}`);
 });
