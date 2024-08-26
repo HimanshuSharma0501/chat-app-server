910475a73ca830456fbf992bc01d2eaa8a8f1031
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-08-26 15:27:34 +0530

    Get Messages Functionality

--- controllers/message.js
+++ controllers/message.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.js";
 import Message from "../models/message.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
@@ -29,6 +30,26 @@ export const sendMessage = async (req, res) => {
     await conversation.save();
     await newMessage.save();
     res.status(200).json({ success: true, newMessage });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+    if (!conversation) return res.status(200).json([]);
+    const messages = conversation.messages;
+    res.status(200).json(messages);
   } catch (error) {
     res.status(500).json({
       success: false,
